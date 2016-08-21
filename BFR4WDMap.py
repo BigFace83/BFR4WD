8b7237df24bc887d644a1bbdce16570da8245811
Big Face <peterneal83@hotmail.co.uk>
2016-08-21 15:21:34 +0000

    Started code for initialising and displaying an occupancy grid map

--- BFR4WDMap.py
+++ BFR4WDMap.py
@@ -22,8 +22,8 @@ class Application(Frame):
         ##################################################################
         # Map Canvas
         ##################################################################
-        self.Map = Canvas(self, width=600, height=600)
-        self.Map.grid(row = 0, column = 0, columnspan = 3, rowspan = 3)
+        self.MapCanvas = Canvas(self, width=600, height=600)
+        self.MapCanvas.grid(row = 0, column = 0, columnspan = 3, rowspan = 3)
   
 
         ##################################################################
@@ -39,6 +39,13 @@ class Application(Frame):
         self.Draw = Button(self, text="Draw Map")
         self.Draw.bind('<Button-1>', self.DrawMap)
         self.Draw.grid(row = 2, column = 4)
+
+        ##################################################################
+        # Change Map button
+        ##################################################################
+        self.Change = Button(self, text="Change")
+        self.Change.bind('<Button-1>', self.ChangeMap)
+        self.Change.grid(row = 3, column = 4)
 
 
     def NewMap(self,event):
@@ -56,25 +63,41 @@ class Application(Frame):
         self.MapArray[:, 80]  =  0.8
         self.MapArray[:, 90]  =  0.9
         self.MapArray[:, 99]  =  1
+        print "New Map Created"
 
+    def ChangeMap(self,event):
+ 
+        self.MapArray[:, 0] =  1
+        self.MapArray[:, 10]  =  0.9
+        self.MapArray[:, 20]  =  0.8
+        self.MapArray[:, 30]  =  0.7
+        self.MapArray[:, 40] =  0.6
+        self.MapArray[:, 50]  =  0.5
+        self.MapArray[:, 60]  =  0.4
+        self.MapArray[:, 70]  =  0.3
+        self.MapArray[:, 80]  =  0.2
+        self.MapArray[:, 90]  =  0.1
+        self.MapArray[:, 99]  =  0
+        print "Map Changed"
  
 
     def DrawMap(self,event):
 
-        self.MapForImage = self.MapArray #copy MapArray and use the copy to modify for displaying
-        for x in np.nditer(self.MapForImage , op_flags=['readwrite']): #Loop through entire array
+        print "Drawing Map"
+        Map = np.copy(self.MapArray)
+        for x in np.nditer(Map , op_flags=['readwrite']): #Loop through entire array
             x[...] = 255-(x* 255) #Convert from a float array (0 to 1) to a grayscale image (0-255)
         
 
-        self.MapForImage  = self.MapForImage .astype(np.uint8) #Make array and array of integers
+        Map  = Map.astype(np.uint8) #Make array and array of integers
 
-        self.MapImage = cv2.cvtColor(self.MapForImage ,cv2.COLOR_GRAY2RGB) #Convert to RGB before adding coloured objects
+        self.MapImage = cv2.cvtColor(Map ,cv2.COLOR_GRAY2RGB) #Convert to RGB before adding coloured objects
         #cv2.circle(self.MapImage, (50, 50), 20, (0,0,255)) #draw a circle
 
         self.MapImage = Image.fromarray(self.MapImage)
         self.MapImage = self.MapImage.resize((600, 600))
 	self.MapImage = ImageTk.PhotoImage(self.MapImage)
-        self.Map .create_image(0,0, anchor=NW, image=self.MapImage)
+        self.MapCanvas .create_image(0,0, anchor=NW, image=self.MapImage)
 
 
 
